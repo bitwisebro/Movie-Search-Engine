1b9934f807d5f93eee3a1f01d5411cc9a0da69d9
GauravJani <34713448+bitwisebro@users.noreply.github.com>
2025-04-04 23:38:21 +0530

    Add some components and pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "Bahubali", release_date: "2020" },
+    { id: 1, title: "3 Idiots", release_date: "1999" },
+    { id: 1, title: "Gangajal", release_date: "2002" },
+  ];
+
+  const handleSearch = () => {};
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for Movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
