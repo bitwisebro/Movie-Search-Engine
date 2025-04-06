efd8248d3b8a91fb3b817babf9d08d70d6bca7e1
GauravJani <34713448+bitwisebro@users.noreply.github.com>
2025-04-06 10:11:58 +0530

    remove unwanted console log

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -4,7 +4,6 @@ import MovieCard from "../components/MovieCard";
 
 function Favorites() {
   const { favorites = [] } = useMovieContext();
-  console.log("hsdifh");
 
   if (favorites && favorites.length > 0)
     return (
